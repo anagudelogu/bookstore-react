9be49ee2f28ae36bc73dd8cdf8f33087df3119b5
Andres Agudelo <anagudelogu@gmail.com>
2022-06-02 17:33:16 -0500

    Rename and move App component to Containers folder as BookstoreApp

---
+++ src/Containers/BookstoreApp/index.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../Navbar';
+import GlobalStyle from '../globalStyles';
+
+const BookstoreApp = () => (
+  <div className="App">
+    <GlobalStyle />
+    <Navbar />
+  </div>
+);
+
+export default BookstoreApp;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import BookstoreApp from './Containers/BookstoreApp';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <BookstoreApp />
+    </BrowserRouter>
   </React.StrictMode>,
 );
