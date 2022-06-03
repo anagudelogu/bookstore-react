c9d78826e663173e3a340480fa199c5fffedaba4
Andres Agudelo <anagudelogu@gmail.com>
2022-06-02 19:45:26 -0500

    Add Categories page with Check status button

--- src/Containers/BookstoreApp/index.jsx
+++ src/Containers/BookstoreApp/index.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from '../Navbar';
 import GlobalStyle from '../globalStyles';
+import Categories from '../Categories';
 
 const BookstoreApp = () => (
   <div className="App">
     <GlobalStyle />
     <Navbar />
+    <Routes>
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
   </div>
 );
 
