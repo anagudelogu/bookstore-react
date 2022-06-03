53bd3a66734a50f8c15e89e8f79cc27aae721271
Andres Agudelo <anagudelogu@gmail.com>
2022-06-02 20:06:04 -0500

    Add Routes to BookstoreApp

--- src/Containers/BookstoreApp/index.jsx
+++ src/Containers/BookstoreApp/index.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from '../Navbar';
 import GlobalStyle from '../globalStyles';
 import Categories from '../Categories';
+import Bookshelf from '../Bookshelf';
 
 const BookstoreApp = () => (
   <div className="App">
     <GlobalStyle />
     <Navbar />
     <Routes>
+      <Route path="/" element={<Bookshelf />} />
       <Route path="/categories" element={<Categories />} />
     </Routes>
   </div>
