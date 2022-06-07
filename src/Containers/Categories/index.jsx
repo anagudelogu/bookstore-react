68b01fff9d3fe73524bc9e9532aea766cafe684a
Andres Agudelo <anagudelogu@gmail.com>
2022-06-07 08:26:58 -0500

    Implement categories check status functionality

--- src/Containers/Categories/index.jsx
+++ src/Containers/Categories/index.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checkStatus } from '../../features/categories/categories';
 
-const Categories = () => <button type="button">Check Status</button>;
+const Categories = () => {
+  const dispatch = useDispatch();
+  const status = useSelector((state) => state.categories.value);
+
+  const handleClick = () => {
+    dispatch(checkStatus('Under Construction'));
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleClick}>
+        Check Status
+      </button>
+      <div>
+        Status:
+        {status}
+      </div>
+    </>
+  );
+};
 
 export default Categories;
