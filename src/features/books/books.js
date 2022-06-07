1b309531d8daccddf2abb8294fe75968d8529759
Andres Agudelo <anagudelogu@gmail.com>
2022-06-07 18:20:52 -0500

    Create a Categories filter, and add more dummy data to store

--- src/features/books/books.js
+++ src/features/books/books.js
@@ -4,8 +4,34 @@ const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
 
 const initialState = [
-  { id: '1', title: 'The Analyst', author: 'John Katzenbatch' },
-  { id: '2', title: "The Madman's Tale", author: 'John Katzenbatch' },
+  {
+    id: '1',
+    title: 'The Analyst',
+    author: 'John Katzenbatch',
+    category: 'Thriller',
+    progress: 76,
+  },
+  {
+    id: '2',
+    title: "The Madman's Tale",
+    author: 'John Katzenbatch',
+    category: 'Thriller',
+    progress: 43,
+  },
+  {
+    id: '3',
+    title: 'Homo Deus',
+    author: 'Yuval Noah Harari',
+    category: 'Social philosophy',
+    progress: 30,
+  },
+  {
+    id: '4',
+    title: 'Twenty Thousand Leagues Under the Sea',
+    author: 'Jules Verne',
+    category: 'Adventure',
+    progress: 97,
+  },
 ];
 
 // Reducer
--- src/features/configureStore.js
+++ src/features/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
+import categoryFilterReducer from './categoryFilter/categoryFilter';
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     categories: categoriesReducer,
+    categoryFilter: categoryFilterReducer,
   },
 });
 
